662bb38b6bece6c6a7b1de208e3f4545a2c80d4b
Miguel Huaranca <miktejere@gmail.com>
2025-03-18 00:20:58 -0400

    funcion de calculo de Exponentes en la rama de Calcular_Potencia

---
+++ CalcularPotencias.py
@@ -0,0 +1,7 @@
+
+base= float(input("Introduce el numero de la base: "))
+exponente= float(input("Introduce el exponente: "))
+
+resultado= base**exponente
+
+print(f"{base} elevado a {exponente} es {resultado}")
